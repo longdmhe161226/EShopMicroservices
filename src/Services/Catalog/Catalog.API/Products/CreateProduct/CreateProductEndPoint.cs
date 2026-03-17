af15ecde03ba2a083f167b95135ac469e791028a
nongnt <nguyenthanhlong16102002@gmail.com>
2026-03-17 17:22:46 +0700

    feat: Add `CreateProduct` endpoint and introduce `BuildingBlocks` project with common dependencies and global usings.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,6 +1,29 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndPoint
+    public record CreateProductRequest(
+        string Name,
+        List<string> Categories,
+        string Description,
+        string ImageFile,
+        decimal Price);
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products",
+                async (CreateProductRequest request, ISender sender) =>
+                {
+                    var command = request.Adapt<CreateProductCommand>();
+                    var result = await sender.Send(command);
+                    var response = result.Adapt<CreateProductResponse>();
+
+                    return Results.Created($"/products/{response.Id}", response);
+                }).WithName("CreateProduct")
+                .WithSummary("Creates a new product")
+                .WithDescription("Creates a new product with the provided details and returns the created product's ID.")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest);
+        }
     }
 }
